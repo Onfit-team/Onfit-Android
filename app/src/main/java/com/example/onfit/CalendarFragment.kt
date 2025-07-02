25351c8f90e0a3dbc1344c4762206c557f51b8d4
MIN2EO <eric91405@gmail.com>
2025-07-02 19:16:27 +0900

    바텀 네비게이션 기능 구현

---
+++ app/src/main/java/com/example/onfit/CalendarFragment.kt
@@ -0,0 +1,6 @@
+package com.example.onfit
+
+import androidx.fragment.app.Fragment
+
+class CalendarFragment : Fragment(R.layout.fragment_calendar){
+}
---
+++ app/src/main/java/com/example/onfit/HomeFragment.kt
@@ -0,0 +1,6 @@
+package com.example.onfit
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
