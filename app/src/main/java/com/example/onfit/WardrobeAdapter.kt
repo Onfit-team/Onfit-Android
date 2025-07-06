4adb527acc8cebadd5b66a00f0ba37e5c8eb0a8a
Dawon-Y <yang20401@naver.com>
2025-07-07 00:51:41 +0900

    옷장 페이지(1차)

---
+++ app/src/main/java/com/example/onfit/WardrobeAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.onfit
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class WardrobeAdapter(private val imageList: List<Int>) :
+    RecyclerView.Adapter<WardrobeAdapter.WardrobeViewHolder>() {
+
+    inner class WardrobeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WardrobeViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_wardrobe, parent, false)
+        return WardrobeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: WardrobeViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageList[position])
+    }
+
+    override fun getItemCount(): Int = imageList.size
+}
