3906db435dfb7c73a44e516a2f00e4c732072389
MIN2EO <eric91405@gmail.com>
2025-07-08 16:01:38 +0900

    feat: 홈 화면 UI 기본 구성

---
+++ app/src/main/java/com/example/onfit/HomeFragment.kt
@@ -0,0 +1,104 @@
+package com.example.onfit
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.onfit.databinding.FragmentHomeBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.onfit.data.model.BestItem
+import com.example.onfit.data.model.SimItem
+
+// fragment_home.xml을 사용하는 HomeFragment 정의
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    //홈 화면 옷 리스트
+    private val clothSuggestList = listOf(
+        R.drawable.cloth1,
+        R.drawable.cloth2,
+        R.drawable.cloth3
+    )
+
+    private val similiarClothList = listOf(
+        SimItem(R.drawable.simcloth1, "딱 좋음"),
+        SimItem(R.drawable.simcloth2, "조금 추움"),
+        SimItem(R.drawable.simcloth3, "많이 더움"),
+        SimItem(R.drawable.simcloth1, "딱 좋음"),
+        SimItem(R.drawable.simcloth2, "조금 추움"),
+        SimItem(R.drawable.simcloth3, "많이 더움")
+    )
+
+    private val latestStyleList = listOf(
+        SimItem(R.drawable.simcloth2, "4월 20일"),
+        SimItem(R.drawable.simcloth3, "4월 19일"),
+        SimItem(R.drawable.simcloth1, "4월 18일"),
+        SimItem(R.drawable.simcloth2, "4월 17일"),
+        SimItem(R.drawable.simcloth3, "4월 16일"),
+        SimItem(R.drawable.simcloth1, "4월 15일")
+    )
+
+    private val bestStyleList = listOf(
+        BestItem(R.drawable.bestcloth1, "TOP 1", "큐야"),
+        BestItem(R.drawable.bestcloth2, "TOP 2", "별이"),
+        BestItem(R.drawable.bestcloth3, "TOP 3", "금이")
+
+
+    )
+
+    //홈 화면 옷 추천 3가지
+    private fun setRandomImages() {
+        val mix = clothSuggestList.shuffled().take(3)
+
+        binding.suggestedCloth1Iv.setImageResource(mix[0])
+        binding.suggestedCloth2Iv.setImageResource(mix[1])
+        binding.suggestedCloth3Iv.setImageResource(mix[2])
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // ViewBinding 객체 연결 (fragment_home.xml의 뷰들과 연결됨)
+        _binding = FragmentHomeBinding.bind(view)
+        val today = LocalDate.now()
+        // 날짜를 "MM월 dd일" 형식으로 포맷 지정
+        val formatter = DateTimeFormatter.ofPattern("MM월 dd일")
+        // 포맷 적용된 문자열로 변환 (ex: "07월 05일")
+        val formattedDate = today.format(formatter)
+        binding.dateTv.text = formattedDate
+
+        //리프레시 버튼 클릭시 이미지 교체
+        binding.refreshIcon.setOnClickListener {
+            setRandomImages()
+        }
+
+        // RecyclerView 어댑터 연결
+        val simadapter = SimiliarStyleAdapter(similiarClothList)
+        binding.similarStyleRecyclerView.adapter = simadapter
+        binding.similarStyleRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        
+        val lateadapter = LatestStyleAdapter(latestStyleList)
+        binding.latestStyleRecyclerView.adapter = lateadapter
+        binding.latestStyleRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        val bestadapter = BestOutfitAdapter(bestStyleList)
+        binding.bestoutfitRecycleView.adapter = bestadapter
+        binding.bestoutfitRecycleView.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        
+        
+        binding.homeRegisterBtn.setOnClickListener { 
+            //여기다가 add 작성
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/onfit/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.onfit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment         // ✅ 여기에 주목
+import androidx.navigation.ui.setupWithNavController
+import com.example.onfit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // NavHostFragment를 직접 가져와서 NavController 얻기
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // BottomNavigationView와 NavController를 연결 → 자동으로 Fragment 전환
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
