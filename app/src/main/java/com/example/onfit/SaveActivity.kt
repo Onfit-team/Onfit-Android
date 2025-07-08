ea3c37717b3d0abfa214b29bd0bd537646f9c209
agnes0827 <agnes20827@gmail.com>
2025-07-08 20:15:35 +0900

    feat: SaveActivity 화면 ui, 기능 구현

--- app/src/main/java/com/example/onfit/SaveActivity.kt
+++ app/src/main/java/com/example/onfit/SaveActivity.kt
@@ -1,20 +1,35 @@
 package com.example.onfit
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.onfit.databinding.ActivitySaveBinding
 
 class SaveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivitySaveBinding
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_save)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySaveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 날짜 정보 RegisterActivity에서 받아오기
+        val dateText = intent.getStringExtra("save_date")
+        binding.saveDateTv.text = dateText
+
+        // 사진 정보 RegisterActivity에서 받아오기
+        val byteArray = intent.getByteArrayExtra("outfit_image")
+        if (byteArray != null) {
+            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+            binding.saveOutfitIv.setImageBitmap(bitmap)
+        }
+
+        // 뒤로가기 버튼
+        binding.saveBackBtn.setOnClickListener {
+            finish()
         }
     }
 }
