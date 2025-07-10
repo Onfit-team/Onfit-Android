f317d25d907bf26e024e5ce8f814250302fe9738
agnes0827 <agnes20827@gmail.com>
2025-07-10 16:03:09 +0900

    feat: OutfitSaveActivity 화면 ui 구성

--- app/src/main/java/com/example/onfit/OutfitRegisterActivity.kt
+++ app/src/main/java/com/example/onfit/OutfitRegisterActivity.kt
@@ -1,15 +1,31 @@
 package com.example.onfit
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.onfit.databinding.ActivityOutfitRegisterBinding
+import com.example.onfit.databinding.ActivitySaveBinding
 
 class OutfitRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var binding: ActivityOutfitRegisterBinding
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_outfit_register)
+        binding = ActivityOutfitRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 아이템 저장 화면으로 이동
+        binding.outfitRegisterSaveBtn.setOnClickListener {
+            val intent = Intent(this, OutfitSaveActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 뒤로가기 버튼
+        binding.outfitRegisterBackBtn.setOnClickListener {
+            finish()
+        }
     }
 }
