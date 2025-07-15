69a2fccd770acf8c7cfa0f724737bce098f4a4c9
agnes0827 <agnes20827@gmail.com>
2025-07-15 11:02:46 +0900

    feat: bottom navigation view 추가

--- app/src/main/java/com/example/onfit/MainActivity.kt
+++ app/src/main/java/com/example/onfit/MainActivity.kt
@@ -17,10 +17,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        // NavHostFragment를 직접 가져와서 NavController 얻기
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // BottomNavigationView와 NavController를 연결 → 자동으로 Fragment 전환
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
