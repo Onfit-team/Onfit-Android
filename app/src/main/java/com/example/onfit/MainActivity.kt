adf1647cf51f1ee0b100a935a4ea3485ee54f186
agnes0827 <agnes20827@gmail.com>
2025-07-16 02:27:34 +0900

    feat: RegisterActivity ui 구현

--- app/src/main/java/com/example/onfit/MainActivity.kt
+++ app/src/main/java/com/example/onfit/MainActivity.kt
@@ -18,11 +18,6 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         // NavHostFragment를 직접 가져와서 NavController 얻기
         val navHostFragment = supportFragmentManager
