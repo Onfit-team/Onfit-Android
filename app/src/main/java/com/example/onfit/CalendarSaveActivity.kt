ba9a7b515cac0549ce758f8f7f9df530a1f637a1
agnes0827 <agnes20827@gmail.com>
2025-07-20 01:41:50 +0900

    feat: CalendarSaveActivity RecyclerView 구현

---
+++ app/src/main/java/com/example/onfit/CalendarSaveActivity.kt
@@ -0,0 +1,35 @@
+package com.example.onfit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.onfit.databinding.ActivityCalendarSaveBinding
+
+class CalendarSaveActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCalendarSaveBinding
+
+    // 액티비티 화면 옷 리스트
+    private val calendarSaveList = listOf(
+        CalendarSaveItem(R.drawable.calendar_save_image2),
+        CalendarSaveItem(R.drawable.calendar_save_image3),
+        CalendarSaveItem(R.drawable.calendar_save_image4),
+        CalendarSaveItem(R.drawable.cloth2)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityCalendarSaveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // RecyclerView 어댑터 연결
+        val calendarSaveAdapter = CalendarSaveAdapter(calendarSaveList)
+        binding.calendarSaveRv.adapter = calendarSaveAdapter
+        binding.calendarSaveRv.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+    }
+}
