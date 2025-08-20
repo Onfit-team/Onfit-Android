9c37a1c2a2ff1e0281d9639d1ee0dabcdb736479
agnes0827 <agnes20827@gmail.com>
2025-08-20 23:39:57 +0900

    feat: 수정

--- app/src/main/java/com/example/onfit/calendar/fragment/CalendarFragment.kt
+++ app/src/main/java/com/example/onfit/calendar/fragment/CalendarFragment.kt
@@ -849,13 +849,13 @@ class CalendarFragment : Fragment() {
                 openCamera()
                 dialog.dismiss()
             }
-            // 갤러리 버튼
-            view.findViewById<LinearLayout>(R.id.gallery_btn).setOnClickListener {
-                ensurePhotoPermission { rescanPicturesAndOpenGallery() }
-                dialog.dismiss()
-            }
-            dialog.show()
         }
+        // 갤러리 버튼
+        view.findViewById<LinearLayout>(R.id.gallery_btn).setOnClickListener {
+            ensurePhotoPermission { rescanPicturesAndOpenGallery() }
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 
         private fun uriToCacheFile(context: Context, uri: Uri): File {
